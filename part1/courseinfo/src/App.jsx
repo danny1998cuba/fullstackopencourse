e7d9be839e75e880000839033f57894f963ca46e
danny1998cuba <danny.glezcuet98@gmail.com>
2025-05-03 21:39:22 -0400

    Part 1a

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,52 @@
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+
+export const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+export const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.part1} exercises={props.exercises1} />
+      <Part name={props.part2} exercises={props.exercises2} />
+      <Part name={props.part3} exercises={props.exercises3} />
+    </div>
+  );
+};
+
+export const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  );
+};
+
+export const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>;
+};
+
+export default App;
