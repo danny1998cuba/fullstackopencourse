a202bd8451b2b383154dbb86083f8940dfc52966
danny1998cuba <danny.glezcuet98@gmail.com>
2025-05-06 17:31:18 -0400

    Part 2b

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,124 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [filterValue, setFilterValue] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    const newNameToSet = newName.trim();
+    const newNumberToSet = newNumber.trim();
+
+    if (newNameToSet !== "" && newNumberToSet !== "") {
+      if (persons.some((p) => p.name === newNameToSet)) {
+        alert(`${newNameToSet} is already added to phonebook`);
+      } else {
+        setPersons([
+          ...persons,
+          {
+            name: newNameToSet,
+            number: newNumberToSet,
+            id: String(persons.length + 1),
+          },
+        ]);
+      }
+      setNewName("");
+      setNewNumber("");
+    }
+  };
+
+  const onFieldChange = (name, event) => {
+    const value = event.target.value;
+    switch (name) {
+      case "name":
+        setNewName(value);
+        break;
+      case "number":
+        setNewNumber(value);
+        break;
+      default:
+        console.log("Invalid field");
+    }
+  };
+
+  const shownPersons =
+    filterValue.trim() !== ""
+      ? persons.filter((p) =>
+          p.name.toLowerCase().includes(filterValue.trim().toLowerCase())
+        )
+      : persons;
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter
+        activeFilter={filterValue}
+        onFilter={(e) => setFilterValue(e.target.value)}
+      />
+
+      <h3>add a new</h3>
+      <PersonForm
+        fieldsValue={{ name: newName, number: newNumber }}
+        onFieldChange={onFieldChange}
+        onSubmit={onSubmit}
+      />
+
+      <h3>Numbers</h3>
+      <Persons persons={shownPersons} />
+    </div>
+  );
+};
+
+const Filter = ({ activeFilter, onFilter }) => {
+  return (
+    <div>
+      filter shown with <input value={activeFilter} onChange={onFilter} />
+    </div>
+  );
+};
+
+const PersonForm = ({ onSubmit, fieldsValue, onFieldChange }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        name:{" "}
+        <input
+          value={fieldsValue["name"]}
+          onChange={(e) => onFieldChange("name", e)}
+        />
+      </div>
+      <div>
+        number:{" "}
+        <input
+          value={fieldsValue["number"]}
+          onChange={(e) => onFieldChange("number", e)}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ persons }) => {
+  return (
+    <div>
+      {persons.map((person) => (
+        <div key={person.id}>
+          {person.name} {person.number}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
